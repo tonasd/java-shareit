1eeca5f0c0e8247bdfa747ad8a6d5775c14975d1
tonasd <tonasd@ya.ru>
2023-05-15 19:15:22 +0300

    add ItemRequest model

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,31 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.practicum.shareit.user.User;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "requests")
+@Getter
+@Setter
+@ToString
 public class ItemRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(length = 2024, nullable = false)
+    String description;
+
+    @Column(nullable = false)
+    Instant created;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    @ToString.Exclude
+    User requester;
 }
--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.request.dto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ItemRequestDto {
+    long id;
+    String description;
+    String created;
 }

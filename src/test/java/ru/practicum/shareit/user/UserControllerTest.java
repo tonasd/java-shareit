6b2f776f878e9373955805fafb054179868e64b7
tonasd <tonasd@ya.ru>
2023-05-21 16:00:03 +0300

    test: add item controller test

--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -7,6 +7,7 @@ import org.mockito.AdditionalAnswers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.user.dto.UserDto;
@@ -49,6 +50,20 @@ class UserControllerTest {
 
         assertEquals(mapper.writeValueAsString(dto), responseBody);
     }
+
+    @SneakyThrows
+    @Test
+    void createNewUser_shouldHaveStatusConflict_whenServiceThrowsDataIntegrityViolationException() {
+        UserDto dto = UserDto.builder().name("name").email("repeatedEmail@mail.net").build();
+        when(service.create(dto)).thenThrow(new DataIntegrityViolationException("EMAIL_UNIQUE violation"));
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(mapper.writeValueAsString(dto)))
+                .andExpect(status().isConflict());
+    }
+
 
     @SneakyThrows
     @Test
